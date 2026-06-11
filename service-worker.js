84042ca1fabf1a58d24b6e8ebf612840c2c0cfaf
lailai950310-rgb <lailai950310-rgb@users.noreply.github.com>
2026-06-11 18:28:48 +0800

    Make mobile hero immersive

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "vitality-fitness-v29";
+const CACHE_NAME = "vitality-fitness-v30";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=29",
+  "./styles.css?v=30",
   "./supabase-config.js?v=20",
-  "./script.js?v=29",
+  "./script.js?v=30",
   "./manifest.webmanifest",
   "./assets/profile-avatar-girl.png",
   "./assets/weight-green-ip.png",
